d3b05b173a5cccfda8232dddf4f966a4b08356d3
Mominov Abu <abuokedavron@gmail.com>
2026-04-30 10:47:20 +0300

    fi

--- C++/script.js
+++ C++/script.js
@@ -1,1 +1,15 @@
-console.log(ism);
+const talaba = {
+    ism:"RAhmatilla",
+    yosh: 20,
+    kursi: 2
+}
+
+const talaba1 = {
+    ism:"RAhmatilla2",
+    yosh: 23,
+    kursi: 3
+}
+
+console.talaba(talaba);
+console.log(talaba.ism);
+console.log(talaba1.ism);
